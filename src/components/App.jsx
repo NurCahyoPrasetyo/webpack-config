78638ee715c1461edcf3fcc607daba4b62d5025b
NurCahyoPrasetiyo <nurcahyoprasetiyo4@gmail.com>
2022-01-10 12:39:41 +0700

    add image suport webpack

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,20 @@
 import "../styles/index.scss";
 import Recipes from "./Recipes";
+import sword from "../images/swc-sword.png";
+import swordSVG from "../images/sword.svg";
 
 function App() {
   return (
     <>
-      <section className="hero">
-        <main>
-          <section>
-            <h1>Oh Hai, React</h1>
-          </section>
-        </main>
+      <section className="hero"></section>
+      <main>
+        <section>
+          <h1>Oh Hai, React</h1>
+        </section>
+        <img src={sword} alt="sword" width={250} />
+        <img src={swordSVG} alt="sword" width={250} />
         <Recipes />
-      </section>
+      </main>
     </>
   );
 }
